21b0c202a5ceca2e2b21a46e362b16472b2ff92b
yaman <yamanjamal09@gmail.com>
2021-09-14 01:33:51 +0300

    houses resource updated

--- app/Http/Resources/House.php
+++ app/Http/Resources/House.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Resources\Json\JsonResource;
 use App\Http\Resources\CommntResource;
 use App\Models\LikesAndDislikes;
+use App\Models\Imge;
 
 class House extends JsonResource
 {
@@ -26,19 +27,19 @@ class House extends JsonResource
             'place'             => $this->place,
             'description'       => $this->description,
             'property_type'     => $this->property_type,    
-            'Balcony'           => $this->Balcony,
-            'Parking'           => $this->Parking,
-            'Pool'              => $this->Pool,
-            'Beach'             => $this->Beach,
-            'Air_condtioning'   => $this->Air_condtioning,
-            'Pet_friendly'      => $this->Pet_friendly,
-            'Kid_friendly'      => $this->Kid_friendly,
-            'Pet_friendly'      => $this->Pet_friendly,
+            // 'Balcony'           => $this->Balcony,
+            // 'Parking'           => $this->Parking,
+            // 'Pool'              => $this->Pool,
+            // 'Beach'             => $this->Beach,
+            // 'Air_condtioning'   => $this->Air_condtioning,
+            // 'Pet_friendly'      => $this->Pet_friendly,
+            // 'Kid_friendly'      => $this->Kid_friendly,
             'rating'            => $this->rating,
-            'likes'=>count(LikesAndDislikes::where('house_id',$this->id)->where('likeState',1)->get()),
-            'dislikes'=>count(LikesAndDislikes::where('house_id',$this->id)->where('likeState',0)->get()),
-            'imeges'               =>ImgeResource::collection( $this->imeges),
-            'comments'          => CommntResource::collection( $this->comments),
+            'likes'             =>count(LikesAndDislikes::where('house_id',$this->id)->where('likeState',1)->get()),
+            'dislikes'          =>count(LikesAndDislikes::where('house_id',$this->id)->where('likeState',0)->get()),
+            'imeges'            =>Imge::where('house_id',$this->id)->first(),
+            // 'comments'          => CommntResource::collection( $this->comments),
+            // 'imeges'               =>ImgeResource::collection( $this->imeges),
             // 'imeges'            => $this->imeges,
         ];
     }
